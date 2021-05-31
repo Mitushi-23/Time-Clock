523fcdc4e03485b37b878fb3904cf540d3be74f0
Mitushi-23 <mitushi2agrawal@gmail.com>
2021-05-31 15:41:51 +0530

    This is a Time Clock

---
+++ USA.js
@@ -0,0 +1,22 @@
+
+
+  let d;
+  let date;
+  let time;
+  let utc;
+  let offset;
+  let nd;
+    
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    
+   
+   
+   setInterval(() => {
+    d = new Date();
+   offset=-4.00;
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    nd = new Date(utc+(3600000*offset));
+  date = nd.toLocaleString(undefined,options);
+   time =  nd.getHours() + ':' + nd.getMinutes() + ':' + nd.getSeconds();
+  document.getElementById('time').innerHTML = time + " <br>on " +date;
+  }, 1000);
